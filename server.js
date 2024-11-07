a480b1f5c7f493f510de34db88ab9bd09f96c22d
Ravi Monreal <ravinog@gmail.com>
2024-11-07 13:56:56 -0300

    Testando ainda

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const port = 3000;
 
 // Define um diretório para servir os arquivos estáticos (HTML, CSS, JS, etc.)
-app.use(express.static('snapshot'));
+app.use(express.static('TRAMPO'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/TRAMPO/index.html');
 });
 
+// Corrigi o fechamento da função app.get
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
